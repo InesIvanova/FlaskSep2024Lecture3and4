8309f793a9359e96a6d8f08a7bb97d206083d739
Ines Ivanova Kenova <ines.ivanova@nmible.com>
2024-09-26 07:40:56 +0300

    Lecture 4 authorization and authentication

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-from datetime import datetime
+import jwt
+
+from datetime import datetime, timedelta
 from enum import Enum
 
 from decouple import config
 from flask import Flask, request
+from flask_httpauth import HTTPTokenAuth
 from flask_migrate import Migrate
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, abort
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow import Schema, fields, ValidationError, validate, validates
 from password_strength import PasswordPolicy
 from sqlalchemy import func
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Mapped, mapped_column
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 api = Api(app)
@@ -25,6 +28,46 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@l
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+auth = HTTPTokenAuth(scheme='Bearer')
+
+
+@auth.verify_token
+def verify_token(token):
+    try:
+        user = User.decode_token(token)
+        return user
+    except jwt.exceptions.InvalidTokenError as ex:
+        abort(401)
+
+
+class UserRolesEnum(Enum):
+    super_admin = "super admin"
+    admin = "admin"
+    user = "user"
+
+
+def permissions_required(required_roles: list[UserRolesEnum]):
+    def decorator(function):
+        def decorated_function(*args, **kwargs):
+            current_user = auth.current_user()
+            if current_user.role in required_roles:
+                abort(403)
+            return function(*args, **kwargs)
+        return decorated_function
+    return decorator
+
+
+def validate_schema(schema):
+    def decorator(function):
+        def decorated_function(*args, **kwargs):
+            schema_obj = schema()
+            data = request.get_json()
+            errors = schema_obj.validate(data)
+            if errors:
+                abort(400, errors=errors)
+            return function(*args, **kwargs)
+        return decorated_function
+    return decorator
 
 class User(db.Model):
     __tablename__ = 'user'
@@ -34,7 +77,29 @@ class User(db.Model):
     full_name: Mapped[str] = mapped_column(db.String(255), nullable=False)
     created_on: Mapped[datetime] = mapped_column(server_default=func.now())
     updated_on: Mapped[datetime] = mapped_column(onupdate=func.now(), server_default=func.now())
+    role: Mapped[UserRolesEnum] = mapped_column(
+        db.Enum(UserRolesEnum),
+        default=UserRolesEnum.user,
+        nullable=False
+    )
 
+    def encode_token(self):
+        key = config("SECRET_KEY")
+        data = {
+            "exp": datetime.utcnow() + timedelta(days=2),
+            "sub": self.id
+        }
+        return jwt.encode(data, key, algorithm="HS256")
+
+    @staticmethod
+    def decode_token(token):
+        key = config("SECRET_KEY")
+        data = jwt.decode(token, key, algorithms=["HS256"])
+        user_id = data["sub"]
+        user = db.session.execute(db.select(User).filter_by(id=user_id)).scalar()
+        if not user:
+            raise jwt.exceptions.InvalidTokenError()
+        return user
 
 class CoffeeEnum(Enum):
     weak = "This coffee type is weak and can be taken even in the evenings"
@@ -62,7 +127,7 @@ class Clothes(db.Model):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(db.String(255), nullable=False)
-    size: Mapped[SizeEnum] =mapped_column(
+    size: Mapped[SizeEnum] = mapped_column(
         db.Enum(SizeEnum),
         default=SizeEnum.s,
         nullable=False
@@ -135,28 +200,40 @@ class UserResponseSchema(BaseUserSchema):
 
 
 class UsersResource(Resource):
+    @validate_schema(UserSignInSchema)
     def post(self):
         data = request.get_json()
-
-        schema = UserSignInSchema()
-        errors = schema.validate(data)
-
-        if not errors:
-            data['password'] = generate_password_hash(data['password'], method='pbkdf2:sha256')
-            user = User(**data)
-            db.session.add(user)
-            try:
-                db.session.commit()
-                response_schema = UserResponseSchema()
-                response_data = response_schema.dump(user)
-                return response_data, 201
-            except IntegrityError as ex:
-                return {"message": "Email already exists, please sign in instead"}, 400
+        headers = request.headers.get("custom")
+        data['password'] = generate_password_hash(data['password'], method='pbkdf2:sha256')
+        user = User(**data)
+        db.session.add(user)
+        try:
+            db.session.commit()
+            token = user.encode_token()
+            return {"token": token}, 201
+        except IntegrityError as ex:
+            return {"mes`sage": "Email already exists, please sign in instead"}, 400
 
         return errors, 400
 
 
+class UserLoginResource(Resource):
+    def post(self):
+        data = request.get_json()
+        # TODO create schema and validate
+        user = db.session.execute(db.select(User).filter_by(email=data["email"])).scalar()
+        if not user:
+            raise Exception("Invalid email or pass")
+
+        result = check_password_hash(user.password, data["password"])
+        if result and user.is_email_verified:
+            return {"token": user.encode_token()}
+        raise Exception("Invalid email or pass")
+
+
 class UserResource(Resource):
+    @auth.login_required
+    @permissions_required([UserRolesEnum.admin, UserRolesEnum.super_admin])
     def get(self, pk):
         user = db.session.execute(db.select(User).filter_by(id=pk)).scalar()
         if not user:
@@ -166,13 +243,5 @@ class UserResource(Resource):
 
 
 api.add_resource(UsersResource, "/register")
+api.add_resource(UserLoginResource, "/login")
 api.add_resource(UserResource, "/users/<int:pk>")
-
-
-class AddressSchema(Schema):
-    street = fields.String()
-    city = fields.String()
-
-class PersonInfoSchema(Schema):
-    name = fields.String()
-    address = fields.Nested(AddressSchema)
